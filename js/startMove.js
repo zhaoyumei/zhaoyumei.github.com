0ee10fe5c69f4b5a5e7b66feab58a94e4b55e9e3
zhao <zym5330@126.com>
2016-11-30 01:16:37 +0800

    change

---
+++ js/startMove.js
@@ -0,0 +1,18 @@
+var iSpeed = 0;
+var left = 0;
+function move(obj,iTarget){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		iSpeed+=(iTarget-obj.offsetLeft)/5;
+		iSpeed*=0.7;
+
+		left+=iSpeed;
+
+		obj.style.left = Math.round(left)+'px';
+
+		if(Math.round(iSpeed) == 0 && Math.round(left) == iTarget){
+			clearInterval(obj.timer);
+		}
+		
+	},30)
+}
